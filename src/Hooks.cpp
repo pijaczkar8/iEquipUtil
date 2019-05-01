f02b6732e3f30287dc0475029ded53e8b13049bd
Ryan-rsm-McKenzie <ryan__mckenzie@hotmail.com>
2019-05-01 13:15:09 -0700

    7.1.1
    * Fixed bug with adding/removing untracked items

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -39,48 +39,48 @@ namespace
 		UInt32* Hook_RemoveItem(UInt32& a_dropHandle, TESForm* a_item, SInt32 a_count, UInt32 a_removeType, BaseExtraList* a_extraList, TESObjectREFR* a_moveToRef, void* a_arg7, void* a_arg8)
 		{
 			auto manager = RefHandleManager::GetSingleton();
-			auto regs = OnRefHandleInvalidatedRegSet::GetSingleton();
+			if (!manager->IsInit() || !manager->IsTrackedType(a_item)) {
+				return (this->*_RemoveItem)(a_dropHandle, a_item, a_count, a_removeType, a_extraList, a_moveToRef, a_arg7, a_arg8);
+			}
+
 #if _WIN64
 			UInt32* ret = g_invalidRefHandle;
 #else
 			UInt32* ret = const_cast<UInt32*>(g_invalidRefHandle);
 #endif
-			if (manager->IsInit()) {
-				if (a_extraList || a_count == 1 || !manager->IsTrackedType(a_item)) {
-					auto result = manager->InvalidateHandle(a_item, a_extraList);
-					ret = (this->*_RemoveItem)(a_dropHandle, a_item, a_count, a_removeType, a_extraList, a_moveToRef, a_arg7, a_arg8);
-					if (result.second) {
-						regs->QueueEvent(a_item, result.first);
-					}
-				} else {
-					std::size_t i = 0;
-					ForEachInvEntry([&](InventoryEntryData* a_entryData) -> bool
-					{
-						if (a_entryData->type == a_item) {
-							while (a_entryData->extendDataList && i < a_count) {
-								auto xLists = reinterpret_cast<RE::BSSimpleList<BaseExtraList*>&>(*a_entryData->extendDataList);
-								if (xLists.empty()) {
-									break;
-								} else {
-									auto result = manager->InvalidateHandle(a_item, xLists.front());
-									ret = (this->*_RemoveItem)(a_dropHandle, a_item, 1, a_removeType, xLists.front(), a_moveToRef, a_arg7, a_arg8);
-									if (result.second) {
-										regs->QueueEvent(a_item, result.first);
-									}
-									++i;
-								}
-							}
-							return false;
-						} else {
-							return true;
-						}
-					});
-					if (i < a_count) {
-						ret = (this->*_RemoveItem)(a_dropHandle, a_item, a_count - i, a_removeType, 0, a_moveToRef, a_arg7, a_arg8);
-					}
+			auto regs = OnRefHandleInvalidatedRegSet::GetSingleton();
+			if (a_extraList || a_count == 1) {
+				auto result = manager->InvalidateHandle(a_item, a_extraList);
+				ret = (this->*_RemoveItem)(a_dropHandle, a_item, a_count, a_removeType, a_extraList, a_moveToRef, a_arg7, a_arg8);
+				if (result.second) {
+					regs->QueueEvent(a_item, result.first);
 				}
 			} else {
-				return (this->*_RemoveItem)(a_dropHandle, a_item, a_count, a_removeType, a_extraList, a_moveToRef, a_arg7, a_arg8);
+				std::size_t i = 0;
+				ForEachInvEntry([&](InventoryEntryData* a_entryData) -> bool
+				{
+					if (a_entryData->type == a_item) {
+						while (a_entryData->extendDataList && i < a_count) {
+							auto xLists = reinterpret_cast<RE::BSSimpleList<BaseExtraList*>&>(*a_entryData->extendDataList);
+							if (xLists.empty()) {
+								break;
+							} else {
+								auto result = manager->InvalidateHandle(a_item, xLists.front());
+								ret = (this->*_RemoveItem)(a_dropHandle, a_item, 1, a_removeType, xLists.front(), a_moveToRef, a_arg7, a_arg8);
+								if (result.second) {
+									regs->QueueEvent(a_item, result.first);
+								}
+								++i;
+							}
+						}
+						return false;
+					} else {
+						return true;
+					}
+				});
+				if (i < a_count) {
+					ret = (this->*_RemoveItem)(a_dropHandle, a_item, a_count - i, a_removeType, 0, a_moveToRef, a_arg7, a_arg8);
+				}
 			}
 
 			return ret;
@@ -90,27 +90,26 @@ namespace
 		void Hook_AddItem(TESForm* a_item, BaseExtraList* a_extraList, SInt32 a_count, TESObjectREFR* a_fromRefr)
 		{
 			auto manager = RefHandleManager::GetSingleton();
-			auto regs = OnRefHandleActiveRegSet::GetSingleton();
+			if (!manager->IsInit() || !manager->IsTrackedType(a_item)) {
+				(this->*_AddItem)(a_item, a_extraList, a_count, a_fromRefr);
+			}
 
-			if (manager->IsInit()) {
-				if (a_extraList) {
-					auto result = manager->ActivateHandle(a_item, a_extraList);
-					(this->*_AddItem)(a_item, a_extraList, a_count, a_fromRefr);
-					if (result.second) {
-						regs->QueueEvent(a_item, result.first, a_count);
-					}
-				} else {
-					for (std::size_t i = 0; i < a_count; ++i) {
-						BaseExtraList* xListOut = 0;
-						auto result = manager->ActivateHandle(a_item, xListOut);
-						(this->*_AddItem)(a_item, xListOut, 1, a_fromRefr);
-						if (result.second) {
-							regs->QueueEvent(a_item, result.first, 1);
-						}
-					}
+			auto regs = OnRefHandleActiveRegSet::GetSingleton();
+			if (a_extraList) {
+				auto result = manager->ActivateHandle(a_item, a_extraList);
+				(this->*_AddItem)(a_item, a_extraList, a_count, a_fromRefr);
+				if (result.second) {
+					regs->QueueEvent(a_item, result.first, a_count);
 				}
 			} else {
-				(this->*_AddItem)(a_item, a_extraList, a_count, a_fromRefr);
+				for (std::size_t i = 0; i < a_count; ++i) {
+					BaseExtraList* xListOut = 0;
+					auto result = manager->ActivateHandle(a_item, xListOut);
+					(this->*_AddItem)(a_item, xListOut, 1, a_fromRefr);
+					if (result.second) {
+						regs->QueueEvent(a_item, result.first, 1);
+					}
+				}
 			}
 		}
 
@@ -118,18 +117,17 @@ namespace
 		void Hook_PickUpItem(TESObjectREFR* a_item, UInt32 a_count, bool a_arg3, bool a_playSound)
 		{
 			auto manager = RefHandleManager::GetSingleton();
-
-			if (manager->IsInit()) {
-				auto result = manager->ActivateHandle(a_item->baseForm, a_item->extraData);
-				TESForm* baseform = a_item->baseForm;
-
+			if (!manager->IsInit() || !manager->IsTrackedType(a_item)) {
 				(this->*_PickUpItem)(a_item, a_count, a_arg3, a_playSound);
+			}
 
-				if (result.second) {
-					OnRefHandleActiveRegSet::GetSingleton()->QueueEvent(baseform, result.first, a_count);
-				}
-			} else {
-				(this->*_PickUpItem)(a_item, a_count, a_arg3, a_playSound);
+			auto result = manager->ActivateHandle(a_item->baseForm, a_item->extraData);
+			TESForm* baseform = a_item->baseForm;
+
+			(this->*_PickUpItem)(a_item, a_count, a_arg3, a_playSound);
+
+			if (result.second) {
+				OnRefHandleActiveRegSet::GetSingleton()->QueueEvent(baseform, result.first, a_count);
 			}
 		}
 
